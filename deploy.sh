1b1ade4fe3669aba3ecfd61df9754d4de6628f98
Just be free <justbefree@126.com>
2020-07-10 16:30:07 +0800

    Auto deploy

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# @Author: Just be free
+# @Date:   2020-07-10 16:26:34
+# @Last Modified by:   Just be free
+# @Last Modified time: 2020-07-10 16:28:15
+
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+git push -f git@github.com:justbefree/justbefree.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
