f293641dee1b83f5ed59470c12641a4aed301ae0
Just be free <justbefree@126.com>
2020-07-10 16:46:28 +0800

    Fixed building error

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,11 +2,10 @@
 * @Author: Just be free
 * @Date:   2020-07-10 09:56:25
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-07-10 14:29:00
+* @Last Modified time: 2020-07-10 16:35:22
 * @E-mail: justbefree@126.com
 */
 const themeConfig = require("./themeConfig");
-console.log(themeConfig);
 module.exports = {
   // title: "yn-components 组件案例文档",
   // description: "前端基础组件技术文档",
@@ -22,12 +21,12 @@ module.exports = {
       lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
       title: 'yn-components',
       description: 'yn-components docs'
-    },
-    '/zh/': {
-      lang: 'zh-CN',
-      title: 'yn-components 组件案例文档',
-      description: '前端基础组件技术文档'
     }
+    // '/zh/': {
+    //   lang: 'zh-CN',
+    //   title: 'yn-components 组件案例文档',
+    //   description: '前端基础组件技术文档'
+    // }
   },
   configureWebpack: {
     resolve: {
--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,11 +2,10 @@
 * @Author: Just be free
 * @Date:   2020-07-10 10:06:14
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-07-10 15:24:38
+* @Last Modified time: 2020-07-10 16:44:29
 * @E-mail: justbefree@126.com
 */
 // 使用异步函数也是可以的
-import YnComponents from "yn-components";
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
@@ -14,6 +13,9 @@ export default ({
   siteData, // 站点元数据
   isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
-  Vue.use(YnComponents);
-  // ...做一些其他的应用级别的优化
+  if(!isServer){
+    import('yn-components/lib/yn-components.umd.min.js').then(module => {
+      Vue.use(module)
+    });  
+  }
 };
