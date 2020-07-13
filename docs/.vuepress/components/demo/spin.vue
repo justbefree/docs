ba8d2985545982d77c29c0d29e45e4baf9d667dd
Just be free <justbefree@126.com>
2020-07-13 10:21:13 +0800

    Added examples for Spin component

---
+++ docs/.vuepress/components/demo/spin.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="spin">
+    <div v-if="demo === '1'">
+      <h4>trip-bounce</h4>
+      <yn-spin type="triple-bounce"></yn-spin>
+    </div>
+    <div v-if="demo === '2'">
+      <h4>snake</h4>
+      <yn-spin type="snake"></yn-spin>
+    </div>
+    <div v-if="demo === '3'">
+      <h4>fading-circle</h4>
+      <yn-spin type="fading-circle"></yn-spin>
+    </div>
+    <div v-if="demo === '4'">
+      <h4>double-bounce</h4>
+      <yn-spin type="double-bounce"></yn-spin>
+    </div>
+    <div v-if="demo === '5'">
+      <h4>rotate</h4>
+      <yn-spin type="rotate"></yn-spin>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DemoSpin",
+  props: {
+    demo: String
+  }
+};
+</script>
