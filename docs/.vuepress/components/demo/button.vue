47b89b090834ca510c82183b9d8ddf124db81f4d
Just be free <justbefree@126.com>
2020-07-14 13:55:14 +0800

    Added examples for button

---
+++ docs/.vuepress/components/demo/button.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="button-box">
+    <yn-button v-if="demo === '1'" class="block" @click="handleClick"><span>这是文案，可点击</span></yn-button>
+    <yn-button v-if="demo === '2'" class="block" disabled><span>禁止点击</span></yn-button>
+    <yn-button v-if="demo === '3'" class="block" :loading="true" loadingColor="red"
+      ><span>这是文案</span></yn-button
+    >
+    <yn-button v-if="demo === '4'" class="block" :loading="true" size="large" loadingColor="green"
+      ><span>这是文案</span></yn-button
+    >
+    <yn-button v-if="demo === '5'" class="block" :loading="true" size="small"
+      ><span>我不可以点击</span></yn-button
+    >
+    <yn-button v-if="demo === '6'" class="block" type="danger">危险</yn-button>
+    <yn-button v-if="demo === '7'" class="block" type="primary">主题色</yn-button>
+    <yn-button v-if="demo === '8'" class="block" type="warning">警告</yn-button>
+    <yn-button v-if="demo === '9'" class="block" type="primary" plain>plain</yn-button>
+    <yn-button v-if="demo === '10'" class="block" type="warning" plain>plain</yn-button>
+    <yn-button v-if="demo === '11'" class="block" :loading="true" loadingText="loading text">plain</yn-button>
+    <yn-button v-if="demo === '12'" class="block" iconName="iconback" iconColor="red">button with icon</yn-button>
+  </div>
+</template>
+<script>
+export default {
+  name: "DemoButton",
+  props: {
+    demo: String
+  },
+  methods: {
+    handleClick() {
+      console.log("接收到click事件");
+      alert("我被点击了~~");
+    }
+  }
+};
+</script>
+<style type="text/css">
+.button-box {
+  text-align: center;
+}
+.block{
+  display: block;
+  margin: 20px 0;
+}
+hr {
+  border: none;
+}
+</style>
--- docs/yui/button.md
+++ docs/yui/button.md
@@ -1,1 +1,50 @@
 # button
+
+<demo-button demo="1"></demo-button>
+```vue
+<yn-button @click="handleClick"><span>这是文案，可点击</span></yn-button>
+```
+<demo-button demo="2"></demo-button>
+```vue
+<yn-button disabled><span>禁止点击</span></yn-button>
+```
+<demo-button demo="3"></demo-button>
+```vue
+<yn-button :loading="true" loadingColor="red"><span>这是文案</span></yn-button>
+```
+<demo-button demo="4"></demo-button>
+```vue
+<yn-button :loading="true" size="large" loadingColor="green"><span>这是文案</span></yn-button>
+```
+<demo-button demo="5"></demo-button>
+```vue
+<yn-button :loading="true" size="small"><span>我不可以点击</span></yn-button>
+```
+<demo-button demo="6"></demo-button>
+```vue
+<yn-button type="danger">危险</yn-button>
+```
+<demo-button demo="7"></demo-button>
+```vue
+<yn-button type="primary">主题色</yn-button>
+```
+<demo-button demo="8"></demo-button>
+```vue
+<yn-button type="warning">警告</yn-button>
+```
+<demo-button demo="9"></demo-button>
+```vue
+<yn-button type="primary" plain>plain</yn-button>
+```
+<demo-button demo="10"></demo-button>
+```vue
+<yn-button type="warning" plain>plain</yn-button>
+```
+<demo-button demo="11"></demo-button>
+```vue
+<yn-button :loading="true" loadingText="loading text">plain</yn-button>
+```
+<demo-button demo="12"></demo-button>
+```vue
+<yn-button iconName="iconback" iconColor="red">button with icon</yn-button>
+```
