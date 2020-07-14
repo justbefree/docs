0668bd0e6a4f154631343d1fa3fdfe5843a81b43
Just be free <justbefree@126.com>
2020-07-14 18:52:18 +0800

    Added examples for datePicker component

---
+++ docs/.vuepress/components/demo/datePicker.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <ul>
+      <li @click="handleClick(1)" v-if="demo === '1'">
+        <yn-button type="primary">默认</yn-button>
+      </li>
+      <li @click="handleClick(2)" v-if="demo === '2'">
+        <yn-button type="primary">指定起止日期</yn-button>
+      </li>
+    </ul>
+    <yn-date-picker v-model="show1" @confirm="handleConfirm"></yn-date-picker>
+    <yn-date-picker
+      v-model="show2"
+      start="1995-03-08"
+      end="2022-03-22"
+      @confirm="handleConfirm"
+    ></yn-date-picker>
+  </div>
+</template>
+<script type="text/javascript">
+export default {
+  name: "DemoDatePicker",
+  props: {
+    demo: String
+  },
+  data() {
+    return {
+      show1: false,
+      show2: false
+    };
+  },
+  methods: {
+    handleConfirm(e) {
+      console.log(e);
+    },
+    handleClick(i) {
+      this[`show${i}`] = !this[`show${i}`];
+    }
+  }
+};
+</script>
+<style type="text/css" scoped="scoped">
+  ul {list-style: none;padding: 0;}
+</style>
+
--- docs/yui/date-picker.md
+++ docs/yui/date-picker.md
@@ -1,1 +1,15 @@
 # date-picker
+
+<demo-datePicker demo="1"></demo-datePicker>
+```vue
+<yn-date-picker v-model="show" @confirm="() => {}"></yn-date-picker>
+```
+<demo-datePicker demo="2"></demo-datePicker>
+```vue
+<yn-date-picker
+  v-model="show"
+  start="1995-03-08"
+  end="2022-03-22"
+  @confirm="() => {}"
+></yn-date-picker>
+```
