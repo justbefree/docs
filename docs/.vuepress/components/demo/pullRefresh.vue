7476e3d505695ff103a1dd7cb6f2f58f0c96e2b1
Just be free <justbefree@126.com>
2020-07-14 18:46:39 +0800

    Added example for pull-refresh

---
+++ docs/.vuepress/components/demo/pullRefresh.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="box" style="border: 2px solid #eee;width: 320px;height: 560px;">
+    <iframe width="320" height="560" style="border: none;" src="https://justbefree.github.io/youdefine-UI/#/pullRefresh"></iframe>
+  </div>
+</template>
