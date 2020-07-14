8548dc1cd83349b0f55b33e54d4a95d236054816
Just be free <justbefree@126.com>
2020-07-14 15:33:00 +0800

    Added example for sticky

---
+++ docs/.vuepress/components/demo/sticky.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="layout-box">
+    <iframe width="320" height="560" src="https://justbefree.github.io/youdefine-UI/#/sticky"></iframe>
+  </div>
+</template>
+<script>
+export default {
+  name: "DemoSticky"
+};
+</script>
