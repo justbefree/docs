7caa0058ba18eccdf403557ca1db4c0b07243f62
Just be free <justbefree@126.com>
2020-07-14 17:32:46 +0800

    Added examples for dropdownMenu

---
+++ docs/.vuepress/components/demo/dropdownMenu.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="box">
+    <iframe width="320" style="border: none" height="560" src="https://justbefree.github.io/youdefine-UI/#/dropdownMenu"></iframe>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: "DemoDropdownMenu"
+};
+</script>
+<style type="text/css" scoped>
+.box {
+  width: 320px;
+  height: 560px;
+  overflow-y: auto;
+  border: 2px solid #eee;
+}
+</style>
