2ecf27a11f3bc42ab716e2166f6625a65e303762
Just be free <justbefree@126.com>
2020-07-21 15:20:58 +0800

    Added updates log list

--- bin/update.js
+++ bin/update.js
@@ -2,18 +2,26 @@
 * @Author: Just be free
 * @Date:   2020-07-21 13:46:22
 * @Last Modified by:   Just be free
-* @Last Modified time: 2020-07-21 14:04:50
+* @Last Modified time: 2020-07-21 15:19:28
 * @E-mail: justbefree@126.com
 */
-
+const fs = require("fs");
+const path = require("path");
 const package = require("../package.json");
 const pkgVersion = package.dependencies["yn-components"].replace("^", "");
 const axios = require("axios");
 const content = `yn-components was updated, the latest version is ${pkgVersion}, you can read online docs for latest updates. https://justbefree.github.io/youdefine-docs/`;
+let data = "";
+try {
+  data = fs.readFileSync("docs/.updates", "utf-8");
+} catch (e) {
+  console.log(e);
+}
+// console.log(`${content}\n${data}`);
 axios.post("https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=7c2c6538-df14-41b3-b45c-ed328de6f7b8", {
-  "msgtype": "text",
-  "text": {
-    "content": content
+  "msgtype": "markdown",
+  "markdown": {
+    "content": `${content}\n${data}`
   }
 })
 .then(response => {
